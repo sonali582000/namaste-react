fbce80b160beb0d4b6d410637f365b6db0952998
sonali582000 <rathodsonali393@gmail.com>
2024-04-19 13:56:42 +0200

    episode-01

---
+++ app.js
@@ -0,0 +1,28 @@
+/*
+<div id="parent">
+  <div id="child">
+    <h1>I'm an H1 tag</h1>
+    <h2>I'm an H2 tag</h2>
+  </div>
+</div>
+*/
+
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I'm an H1 tag"),
+    React.createElement("h2", {}, "I'm an H2 tag"),
+  ]),
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I'm an H1 tag"),
+    React.createElement("h2", {}, "I'm an H2 tag"),
+  ]),
+]);
+
+// const heading = React.createElement(
+//   "h1",
+//   { id: "heading" },
+//   "Hello World from React !!"
+// );
+console.log(parent);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
